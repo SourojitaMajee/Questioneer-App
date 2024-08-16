0bc420e0fd510cce2806656b4cb6b2714219d9a0
Sourojita <smm64740@gmail.com>
2024-08-16 17:22:22 +0530

    otification routes and controllers

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -103,14 +103,12 @@ export const likeUnlikePost = async (req, res) => {
     if (userLikedPost) {
       // Unlike post
       await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
-      await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
-
-      const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
-      res.status(200).json(updatedLikes);
+      await User.updateOne({ _id: postId }, { $pull: { likedPosts: postId } });
+      res.status(200).json({ message: "Post unliked successfully" });
     } else {
       // Like post
       post.likes.push(userId);
-      await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } });
+      await User.updateOne({ _id: postId }, { $puah: { likedPosts: postId } });
       await post.save();
 
       const notification = new Notification({
@@ -120,8 +118,7 @@ export const likeUnlikePost = async (req, res) => {
       });
       await notification.save();
 
-      const updatedLikes = post.likes;
-      res.status(200).json(updatedLikes);
+      res.status(200).json({ message: "Post liked successfully" });
     }
   } catch (error) {
     console.log("Error in likeUnlikePost controller: ", error);
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -28,6 +29,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
